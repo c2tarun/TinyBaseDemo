60ab3c15e531fbea402e28a2de2ace4f329a3423
Tarun K. Mall <c2tarun@gmail.com>
2024-03-02 06:17:50 -0800

    Configuring tinybase.

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import { useTinyBaseDevTools } from '@dev-plugins/tinybase';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { useCounterStore } from './store';
 
 export default function App() {
   const counter = useCounterStore((state) => state.count);
+  useTinyBaseDevTools(useCounterStore);
   return (
     <View style={styles.container}>
       <Text>{counter}</Text>
